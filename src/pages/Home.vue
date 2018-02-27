a262476873f03410ad50dd22a088f7bcd23edaf5
Amenallah Hsoumi <amenallah.hsoumi@gmail.com>
2018-02-27 18:01:29 +0100

    intial home page with fab button

---
+++ src/pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12>
+      <v-btn
+        v-show="loggedIn"
+        absolute
+        dark
+        fab
+        bottom
+        right
+        color="purple"
+        to="/create"
+        class="mb-5"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: 'home',
+
+    computed: {
+      ...mapGetters('user', {
+        loggedIn: 'loggedIn'
+      })
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Home from '@/pages/Home';
+
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    }
   ]
 })
